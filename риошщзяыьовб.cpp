05d8c1f5df75fd0dbb77e3dd509de927e640b5f8
student <student@C7A109RU-14NN>
2025-02-14 19:28:42 +0300

    Добавьте файлы проекта.

---
+++ риошщзяыьовб.cpp
@@ -0,0 +1,14 @@
+﻿// риошщзяыьовб.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World!\n";
+    cout << "Have a nice day!" << endl;
+}
+
+
